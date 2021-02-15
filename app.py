bc74f696d8886526b95213d24a972075c5a2fae8
julian <julian.harley@gmail.com>
2021-02-15 21:39:21 +0000

    first commit - deploy to heroku

---
+++ app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+# run with
+# streamlit run <SCRIPT.py>
+
+def main():
+    st.title('Votes for women - uk ')
+
+
+
+if __name__ == '__main__':
+    main()
