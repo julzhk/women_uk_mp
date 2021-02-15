06d3165d9c822d4718c5ad79129bf32a44e3efa8
julian <julian.harley@gmail.com>
2021-02-15 22:33:42 +0000

    add a simple chart

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 import pandas as pd
-import numpy as np
 import streamlit as st
+
 
 # run with
 # streamlit run <SCRIPT.py>
 
 def load_data():
     CSV_DATA_PATH = 'most-votes-for-women.tsv'
-    data = pd.read_csv(CSV_DATA_PATH,delimiter='\t').fillna(value=0)
+    data = pd.read_csv(CSV_DATA_PATH, delimiter='\t').fillna(value=0)
     return data
+
 
 def main():
     st.title('Votes for women - UK')
     df = load_data().transpose()
-    df['total'] = df.sum(axis=1)
+    df['total'] = df[3:].sum(axis=1)
 
     if st.checkbox('Show raw data'):
         st.subheader('Raw data')
         st.write(df)
+    st.bar_chart(df['total'])
 
 
 if __name__ == '__main__':
