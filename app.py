5f0c2dc5e9f0a8df9b0400d46d0c3636b01acb17
julian <julian.harley@gmail.com>
2021-02-15 21:56:41 +0000

    show some data

--- app.py
+++ app.py
@@ -5,9 +5,19 @@ import streamlit as st
 # run with
 # streamlit run <SCRIPT.py>
 
-def main():
-    st.title('Votes for women - uk ')
+def load_data():
+    CSV_DATA_PATH = 'most-votes-for-women.tsv'
+    data = pd.read_csv(CSV_DATA_PATH,delimiter='\t').fillna(value=0)
+    return data
 
+def main():
+    st.title('Votes for women - UK')
+    df = load_data().transpose()
+    df['total'] = df.sum(axis=1)
+
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(df)
 
 
 if __name__ == '__main__':
